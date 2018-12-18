adc70b9eed6db2a5f4a1af92dbd94436c630ffaf
NomadRiding <fundora.a@gmail.com>
2018-12-18 14:05:08 -0500

    migrate, create require params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Articles.new(params[:article])
+    @article.save
+      redirect_to @article
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
